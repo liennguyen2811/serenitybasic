338ddf524537b3e56e548925fbc387c6fec4679e
liennth7 <liennth7@unifiedpost.com>
2020-02-25 11:04:10 +0700

    update

--- src/test/java/org/fpt/automation/pages/HomePage.java
+++ src/test/java/org/fpt/automation/pages/HomePage.java
@@ -3,6 +3,6 @@ package org.fpt.automation.pages;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("https://console.developers.google.com/apis")
+
 public class HomePage extends PageObject {
 }
