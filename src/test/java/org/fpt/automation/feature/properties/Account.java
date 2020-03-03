01b428e5708873328ad91c90d629868b88c2e693
liennth7 <liennth7@unifiedpost.com>
2020-03-03 16:51:26 +0700

    REvert

---
+++ src/test/java/org/fpt/automation/feature/properties/Account.java
@@ -0,0 +1,26 @@
+package org.fpt.automation.feature.properties;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Account {
+    private String fullName;
+    private String email;
+    private String phone;
+    private String password;
+
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    public void setPassword(String rePassword) {
+        this.fullName = this.password;
+    }
+
+    public Account(String fullName, String email, String phone, String password) {
+        this.fullName = fullName;
+        this.email = String.format("%s_%s", email, RandomStringUtils.randomAlphabetic(20));
+        this.password = password;
+        this.phone = phone;
+    }
+
+}
