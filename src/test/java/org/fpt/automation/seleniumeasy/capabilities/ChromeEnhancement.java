7786e8d46540ee6f866832c9cebe8bd0f589cffe
liennth7 <liennth7@unifiedpost.com>
2020-03-06 13:57:04 +0700

    issue

--- src/test/java/org/fpt/automation/seleniumeasy/capabilities/ChromeEnhancement.java
+++ src/test/java/org/fpt/automation/seleniumeasy/capabilities/ChromeEnhancement.java
@@ -4,15 +4,27 @@ import net.serenitybdd.core.webdriver.enhancers.BeforeAWebdriverScenario;
 import net.thucydides.core.model.TestOutcome;
 import net.thucydides.core.util.EnvironmentVariables;
 import net.thucydides.core.webdriver.SupportedWebDriver;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class ChromeEnhancement implements BeforeAWebdriverScenario {
     @Override
-    public DesiredCapabilities apply(EnvironmentVariables environmentVariables, SupportedWebDriver supportedWebDriver, TestOutcome testOutcome,DesiredCapabilities capabilities){
+    public DesiredCapabilities apply(EnvironmentVariables environmentVariables,
+                                     SupportedWebDriver driver,
+                                     TestOutcome testOutcome,
+                                     DesiredCapabilities capabilities) {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
-        capabilities.setCapability(ChromeOptions.CAPABILITY,options);
-        return  capabilities;
+        options.addArguments("--window-size=600,600");
+        options.addArguments("--window-position=300,300");
+
+
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        capabilities.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+        capabilities.setCapability("name", testOutcome.getStoryTitle() + " - " + testOutcome.getTitle());
+
+        return capabilities;
     }
 }
